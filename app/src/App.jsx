cd137c5e64d2c0b7cfb9ef9b2fbe4c648a72864e
Uzy777 <usmon.h@outlook.com>
2026-01-04 14:01:39 +0000

    Deploy React hello world

---
+++ app/src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div style={{ padding: 24, fontFamily: "system-ui" }}>
+      <h1>Hello World!</h1>
+      <p>This is React running on GitHub Pages.</p>
+    </div>
+  )
+}
